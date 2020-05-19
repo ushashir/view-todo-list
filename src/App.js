11c5034e40bbc3be24894108cba75fe27bba5619
Ushahemba Shir <laleoict@gmail.com>
2020-05-19 22:33:14 +0100

    user details props destructured manually

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import './App.css';
+// import './App.css';
 import View from './components/View';
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-// import Hello from './Hello';
+// import './index.css';
+import View from './components/View'
+// import App from './App';
+import 'tachyons';
+import { users } from './components/users';
+
 
 ReactDOM.render(
-    <App />,
+  <div>
+    <View id={users[0].id} name={users[0].name} username={users[0].username} email={users[0].email} phone={users[0].phone} website={users[0].website} />
+    <View id={users[1].id} name={users[1].name} username={users[1].username} email={users[1].email} phone={users[1].phone} website={users[1].website} />
+    <View id={users[2].id} name={users[2].name} username={users[2].username} email={users[2].email} phone={users[2].phone} website={users[2].website} />
+   
+  </div>,
+
   document.getElementById('root')
 );
 
