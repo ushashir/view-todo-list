2dd6cc023ea7098cde184ce9a1d6af529f94bd81
Ushahemba Shir <laleoict@gmail.com>
2020-05-19 22:40:23 +0100

    user details

--- src/components/View.js
+++ src/components/View.js
@@ -2,19 +2,19 @@ import React from 'react';
 // import users from './users'
 
 
-const View = ( props ) => {
-    const { id, name, username, email, phone, website } = props;
+const View = ( { id, name, username, email, phone, website } ) => {
+
     return (
     
          <div className="bg-light-light dib br3 pa3 ma2 grow bw2 shadow-5">  
                 <div>
-                    <h1>User Details</h1>
-                   <p>{ id } </p>
-                   <p>{ name } </p>
-                   <p>{ username } </p>
-                   <p>{ email } </p>
-                   <p>{ phone } </p>
-                   <p>{ website } </p>
+                    <h2>USER DETAILS</h2>
+                   <p>ID: { id } </p>
+                   <p> Nane: { name } </p>
+                   <p> Username: { username } </p>
+                   <p> Email: { email } </p>
+                   <p> Phone: { phone } </p>
+                   <p> Website: { website } </p>
                  
                 </div>  
            </div>
