0a9d48f2a04787de5ebeebaad098a5efa91e1882
Ushahemba Shir <laleoict@gmail.com>
2020-05-22 15:21:48 +0100

    login state created

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import ViewList from './components/ViewList';
 import SearchBox from './components/SearchBox';
 import Scroll from './components/Scroll';
-// import Login from './components/Login'
+import Login from './components/Login'
 
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
+            route: 'login',
             users: [], 
             searchField: ''
         }
@@ -36,10 +37,15 @@ class App extends Component {
                 <div className='tc'>
                     <img alt="logo" src={ require('./logo.png') } />
                     <h1>SEARCH TODO LIST APP</h1>
-                    <SearchBox searchChange={this.onsearchChange}/>
-                    <Scroll>
-                        <ViewList users={filteredUsers}/>
-                    </Scroll>
+                    {this.state.route === 'login' ? <Login /> :
+                        <div> 
+                            <SearchBox searchChange={this.onsearchChange}/>
+                            <Scroll>
+                                <ViewList users={filteredUsers}/>
+                            </Scroll>
+                        </div>
+                        }
+                        
                 </div>
             )
         }  
--- src/components/Login.js
+++ src/components/Login.js
@@ -41,7 +41,7 @@ class SignIn extends React.Component {
                 <main className="pa4 black-80">
                     <div className="measure center">
                         <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                            <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+                            <legend className="f2 fw6 ph0 mh0">Login with Twitter</legend>
                             <div className="mt3">
                                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                                 <input onChange = {this.onEmailChange}className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
@@ -52,10 +52,7 @@ class SignIn extends React.Component {
                             </div>
                             <div className="">
                             <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                            type="submit" value="Sign in" onClick = {this.onSubmitSignIn}/>
-                            </div>
-                            <div className="lh-copy mt3">
-                            <p onClick = {() => onRouteChange("register")} className="f6 link dim black db pointer">Register</p>
+                            type="submit" value="Login" onClick = {this.onSubmitSignIn}/>
                             </div>
                         </fieldset>
                     </div>
